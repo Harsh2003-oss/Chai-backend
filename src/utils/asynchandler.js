e9921768f113c558e22cf0cb5f6d0aa72718f669
Harsh2003-oss <harsh.sil872@gmail.com>
2025-07-28 00:04:18 +0530

    updated utils

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -1,13 +1,12 @@
-const asyncHandler = (fn) =>async(req,res,next)=>{
+const  asyncHandler = require("vite/module-runner")
 
-try {
-    await fn(req,res,next)
-} catch (error) {
-    res.status(error.code || 500).json({
-        success:false,
-        message: error.message
-    })
-}
-}
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch
+        ((err) => next(err))
+    }
+}  
 
-module.exports = asyncHandler
+
+
+module.exports = asyncHandler;
